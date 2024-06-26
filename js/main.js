5f156c0abb0d6b1670fdfc04add29ecd4f24ed48
KevinTitanZ <138833988+KevinTitanZ@users.noreply.github.com>
2024-06-25 21:22:09 -0500

    Nav2
    Casi

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@ const togglePassword = button => {
 
     input.type = input.type === "password" ? "text" : "password";
 };
+
+
+
+function validateLoginForm() {
+    var username = document.querySelector('input[type="text"]').value;
+    var password = document.querySelector('input[type="password"]').value;
+    
+    if (username.trim() === '' || password.trim() === '') {
+        alert('Por favor, llena todos los campos.');
+        return false; // Evita que el formulario se envíe
+    }
+    
+    // Si todo está bien, el formulario se enviará normalmente
+    return true;
+}
